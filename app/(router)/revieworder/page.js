df4cba39db53aa40e4052a21e145b76954569518
Usama7962 <usamaafzal489@gmail.com>
2025-09-19 03:53:35 -0700

    Add Revieworder and Review components, update Address and Addtocart modules

---
+++ app/(router)/revieworder/page.js
@@ -0,0 +1,12 @@
+import Revieworder from '@/app/Modules/Revieworder/Revieworder'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Revieworder/>
+    </div>
+  )
+}
+
+export default page
--- app/Modules/Address/Address.js
+++ app/Modules/Address/Address.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ShoppingAddress from './ShoppingAddress'
+import Navbar from '@/app/component/Navbar/Navbar'
+import Footer from '@/app/component/Footer/Footer'
 
 const Address = () => {
   return (
     <>
+    <Navbar/>
      <ShoppingAddress/>
+     <Footer/>
     </>
   )
 }
